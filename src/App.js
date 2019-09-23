528708b91cda2771f4b8c982b368126727f55498
gabrielccarvalho <caampos22@gmail.com>
2019-09-22 22:32:12 -0300

    chore: add header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from './styles/global';
-
+import Header from './components/Header';
 import Routes from './routes';
 
 // import { Container } from './styles';
@@ -10,6 +10,7 @@ import Routes from './routes';
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <GlobalStyles />
     </BrowserRouter>
