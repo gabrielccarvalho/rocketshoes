0dc6add920752174e3bf17e56776fb4d26b4d780
gabrielccarvalho <caampos22@gmail.com>
2019-09-22 23:41:19 -0300

    chore: add Redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyles from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
-// import { Container } from './styles';
+import store from './store';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyles />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyles />
+      </BrowserRouter>
+    </Provider>
   );
 }
